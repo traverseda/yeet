ecfbaebba07629f59be6b32022bd3a28b9ea2c16
Alex Davies <traverse.da@gmail.com>
2023-05-14 13:29:45 -0300

    Got rid of some old code, changed warcdb license

--- yeet/warcdb.py
+++ yeet/warcdb.py
@@ -2,6 +2,20 @@
 
 We're not using real warc for a few reasons, mostly because we'd also need
 to maintain a CDX file and at that point why not just through it in sqlite.
+
+This file (and this file alone) is licensed under the MIT license instead of yeet's broader AGPL license,
+in an effort to provide better interopobility with commercial projects. If there's sufficient interest in splitting
+this off into it's own pip package I will, but unless you have an actual project
+this like 100+ users probably just copy this file out of the repo.
+
+Copyright 2023 Alex Davies <traverse.da@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 """
 from peewee import SmallIntegerField, SqliteDatabase, Model
 from peewee import Model, SqliteDatabase, CharField, TextField, BlobField, AutoField, ForeignKeyField, DateTimeField, CompositeKey
